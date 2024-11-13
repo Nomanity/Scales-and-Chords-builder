5f460aa0049f4599424deac4d8a35aea53289637
Nomanity <leovski76@gmail.com>
2024-11-13 09:18:52 +0500

    Add user input and intro text

--- Code.py
+++ Code.py
@@ -1,7 +1,23 @@
 notes = ('C', 'C♯', 'D', 'D♯','E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B')
 class Scale:
+    def __init__(self, note):
+        self.note = user_input
     pass
 class Chord:
+    def __init__(self, note):
+        self.note = user_input
     pass
 class GuitarNotation:
     pass
+
+print ('In modern music there are 12 notes:', str(notes), 'Any scale or chord is builded from these notes with specific formula.', 'For example, the formula for major scale is: root, whole, whole, half, whole, whole, whole, half', sep = '\n')
+
+pause_input = input(' ')
+user_input = input('Please, choose the root note: ')
+while user_input not in notes:
+    print ('There isn\'t such note')
+    user_input = input('Please, choose the root note: ')
+
+scale_or_chord = input('Will it be a scale or a chord: ')
+
+
